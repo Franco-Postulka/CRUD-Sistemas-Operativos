7f7dcb3f158ba332a26aba02287c44a10d4af3de
Franco-Postulka <francopostulka@hotmail.com>
2024-06-29 13:02:08 -0300

    Funcion para modificar y eliminar sistema de la DB

--- ADO/AccesoDatos.cs
+++ ADO/AccesoDatos.cs
@@ -81,42 +81,44 @@ namespace ADO
 
                 while (lector.Read())
                 {
+                    int tipo = (int)lector["tipo"];
+                    switch (tipo)
+                    {
+                        case 0:
+                            //windows
+                            Windows windows = new Windows();
+                            windows.Nombre = lector["nombre"].ToString();
+                            windows.Version = lector["version"].ToString();
+                            windows.EspacioGB = (float)lector["espacioGB"];
+                            windows.Soporte = (EEstadoSoporte)lector["estadoSoporte"];
+                            windows.Edicion = (EEdicionWindows)lector["edicion"];
+                            windows.VirtualizacionPermitida = (bool)lector["virtualizacionPermitida"];
+                            lista.Add(windows);
+                            break;
+                        case 1:
 
-                    if ((int)lector["tipo"] == 0)
-                    {
-                        //windows
-                        Windows sistema = new Windows();
-                        sistema.Nombre = lector["nombre"].ToString();
-                        sistema.Version = lector["version"].ToString();
-                        sistema.EspacioGB = (float)lector["espacioGB"];
-                        sistema.Soporte = (EEstadoSoporte)lector["estadoSoporte"];
-                        sistema.Edicion = (EEdicionWindows)lector["edicion"];
-                        sistema.VirtualizacionPermitida = (bool)lector["virtualizacionPermitida"];
-                        lista.Add(sistema);
-                    }
-                    else if ((int)lector["tipo"] == 1)
-                    {
-                        //mac
-                        MacOS sistema = new MacOS();
-                        sistema.Nombre = lector["nombre"].ToString();
-                        sistema.Version = lector["version"].ToString();
-                        sistema.EspacioGB = (float)lector["espacioGB"];
-                        sistema.Soporte = (EEstadoSoporte)lector["estadoSoporte"];
-                        sistema.CompatibleConProcesadorApple = (bool)lector["compatibleConProcesadorApple"];
-                        sistema.IntegracionIcloud = (bool)lector["integracionIcloud"];
-                        lista.Add(sistema);
-                    }
-                    else if ((int)lector["tipo"] == 2) 
-                    { 
-                        //linux
-                        Linux sistema = new Linux();
-                        sistema.Nombre = lector["nombre"].ToString();
-                        sistema.Version = lector["version"].ToString();
-                        sistema.EspacioGB = (float)lector["espacioGB"];
-                        sistema.Soporte = (EEstadoSoporte)lector["estadoSoporte"];
-                        sistema.Distribucion = (EDistribucionLinux)lector["distribucion"];
-                        sistema.InterfazGrafica = (bool)lector["interfazGrafica"];
-                        lista.Add(sistema);
+                            //mac
+                            MacOS mac = new MacOS();
+                            mac.Nombre = lector["nombre"].ToString();
+                            mac.Version = lector["version"].ToString();
+                            mac.EspacioGB = (float)lector["espacioGB"];
+                            mac.Soporte = (EEstadoSoporte)lector["estadoSoporte"];
+                            mac.CompatibleConProcesadorApple = (bool)lector["compatibleConProcesadorApple"];
+                            mac.IntegracionIcloud = (bool)lector["integracionIcloud"];
+                            lista.Add(mac);
+                            break;
+                        case 2:
+                            //linux
+                            Linux sistema = new Linux();
+                            sistema.Nombre = lector["nombre"].ToString();
+                            sistema.Version = lector["version"].ToString();
+                            sistema.EspacioGB = (float)lector["espacioGB"];
+                            sistema.Soporte = (EEstadoSoporte)lector["estadoSoporte"];
+                            sistema.Distribucion = (EDistribucionLinux)lector["distribucion"];
+                            sistema.InterfazGrafica = (bool)lector["interfazGrafica"];
+                            lista.Add(sistema);
+                            break;
+
                     }
                 }
                 lector.Close();
@@ -141,7 +143,7 @@ namespace ADO
 
         #region Insert
 
-        public bool AgregarDato(SistemaOperativo sistema)
+        public bool AgregarSistema(SistemaOperativo sistema)
         {
             bool rta = true;
 
@@ -161,8 +163,9 @@ namespace ADO
                     Windows windows = (Windows)sistema;
                     this.comando.Parameters.AddWithValue("@edicion", (int)windows.Edicion);
                     this.comando.Parameters.AddWithValue("@virtualizacionPermitida", windows.VirtualizacionPermitida);
+                    this.comando.Parameters.AddWithValue("@tipo", (int)ETipoSistema.Windows);
 
-                    sql = "INSERT INTO tabla (nombre, version, espacioGB, estadoSoporte, edicion, virtualizacionPermitida) VALUES (@nombre, @version, @espacioGB, @estadoSoporte, @edicion, @virtualizacionPermitida)";
+                    sql = "INSERT INTO tabla (nombre, version, espacioGB, estadoSoporte, edicion, virtualizacionPermitida, tipo) VALUES (@nombre, @version, @espacioGB, @estadoSoporte, @edicion, @virtualizacionPermitida, @tipo)";
 
                 }
                 else if(sistema.GetType() == typeof(Linux))
@@ -170,8 +173,10 @@ namespace ADO
                     Linux linux = (Linux)sistema;
                     this.comando.Parameters.AddWithValue("@distribucion", (int)linux.Distribucion);
                     this.comando.Parameters.AddWithValue("@interfazGrafica", linux.InterfazGrafica);
+                    this.comando.Parameters.AddWithValue("@tipo", (int)ETipoSistema.Linux);
 
-                    sql = "INSERT INTO tabla (nombre, version, espacioGB, estadoSoporte, distribucion, interfazGrafica) VALUES (@nombre, @version, @espacioGB, @estadoSoporte, @distribucion, @interfazGrafica)";
+
+                    sql = "INSERT INTO tabla (nombre, version, espacioGB, estadoSoporte, distribucion, interfazGrafica, tipo) VALUES (@nombre, @version, @espacioGB, @estadoSoporte, @distribucion, @interfazGrafica, @tipo)";
 
                 }
                 else if (sistema.GetType() == typeof(MacOS))
@@ -179,8 +184,10 @@ namespace ADO
                     MacOS mac = (MacOS)sistema;
                     this.comando.Parameters.AddWithValue("@integracionIcloud", mac.IntegracionIcloud);
                     this.comando.Parameters.AddWithValue("@compatibleConProcesadorApple", mac.CompatibleConProcesadorApple);
+                    this.comando.Parameters.AddWithValue("@tipo", (int)ETipoSistema.MacOS);
 
-                    sql = "INSERT INTO tabla (nombre, version, espacioGB, estadoSoporte, integracionIcloud, compatibleConProcesadorApple) VALUES (@nombre, @version, @espacioGB, @estadoSoporte, @integracionIcloud, @compatibleConProcesadorApple)";
+
+                    sql = "INSERT INTO tabla (nombre, version, espacioGB, estadoSoporte, integracionIcloud, compatibleConProcesadorApple, tipo) VALUES (@nombre, @version, @espacioGB, @estadoSoporte, @integracionIcloud, @compatibleConProcesadorApple, @tipo)";
 
                 }
 
@@ -213,106 +220,137 @@ namespace ADO
             return rta;
         }
 
-        //#endregion
+        #endregion
 
-        //#region Update
+        #region Update
 
-        ///// <summary>
-        ///// Modifica un elemento de la DB por ID, si el id no existe o por algun otra razon no modificó el elemento, retorna false
-        ///// </summary>
-        ///// <param name="param"></param>
-        ///// <returns></returns>
-        //public bool ModificarDato(Dato param)
-        //{
-        //    bool rta = true;
+        /// <summary>
+        /// Modifica un elemento de la DB por ID, si el id no existe o por algun otra razon no modificó el elemento, retorna false
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns></returns>
+        public bool ModificarSistema(SistemaOperativo sistema)
+        {
+            bool rta = true;
+            try
+            {
+                this.comando = new SqlCommand();
 
-        //    try
-        //    {
-        //        this.comando = new SqlCommand();
+                this.comando.Parameters.AddWithValue("@nombre", sistema.Nombre);
+                this.comando.Parameters.AddWithValue("@version", sistema.Version);
+                this.comando.Parameters.AddWithValue("@espacioGB", sistema.EspacioGB);
+                this.comando.Parameters.AddWithValue("@estadoSoporte", (int)sistema.Soporte);
 
-        //        this.comando.Parameters.AddWithValue("@id", param.id);
-        //        this.comando.Parameters.AddWithValue("@cadena", param.cadena);
-        //        this.comando.Parameters.AddWithValue("@entero", param.entero);
-        //        this.comando.Parameters.AddWithValue("@flotante", param.flotante);
+                string sql = "UPDATE tabla ";
+                sql += "SET nombre = @nombre, version = @version, espacioGB = @espacioGB, estadoSoporte = @estadoSoporte ";
+               
+                if (sistema.GetType() == typeof(Windows))
+                {
+                    Windows windows = (Windows)sistema;
+                    this.comando.Parameters.AddWithValue("@edicion", (int)windows.Edicion);
+                    this.comando.Parameters.AddWithValue("@virtualizacionPermitida", windows.VirtualizacionPermitida);
+                    this.comando.Parameters.AddWithValue("@tipo", (int)ETipoSistema.Windows);
 
-        //        string sql = "UPDATE dato ";
-        //        sql += "SET cadena = @cadena, entero = @entero, flotante = @flotante ";
-        //        sql += "WHERE id = @id";
+                    sql += ", edicion = @edicion, virtualizacionPermitida = @virtualizacionPermitida, tipo = @tipo";
 
-        //        this.comando.CommandType = CommandType.Text;
-        //        this.comando.CommandText = sql;
-        //        this.comando.Connection = this.conexion;
+                }
+                else if (sistema.GetType() == typeof(Linux))
+                {
+                    Linux linux = (Linux)sistema;
+                    this.comando.Parameters.AddWithValue("@distribucion", (int)linux.Distribucion);
+                    this.comando.Parameters.AddWithValue("@interfazGrafica", linux.InterfazGrafica);
+                    this.comando.Parameters.AddWithValue("@tipo", (int)ETipoSistema.Linux);
 
-        //        this.conexion.Open();
+                    sql += ", distribucion = @distribucion, interfazGrafica = @interfazGrafica, tipo = @tipo";
 
-        //        int filasAfectadas = this.comando.ExecuteNonQuery();
+                }
+                else if (sistema.GetType() == typeof(MacOS))
+                {
+                    MacOS mac = (MacOS)sistema;
+                    this.comando.Parameters.AddWithValue("@integracionIcloud", mac.IntegracionIcloud);
+                    this.comando.Parameters.AddWithValue("@compatibleConProcesadorApple", mac.CompatibleConProcesadorApple);
+                    this.comando.Parameters.AddWithValue("@tipo", (int)ETipoSistema.MacOS);
 
-        //        if (filasAfectadas == 0)
-        //        {
-        //            rta = false;
-        //        }
+                    sql += ", integracionIcloud = @integracionIcloud, compatibleConProcesadorApple = @compatibleConProcesadorApple, tipo = @tipo";
 
-        //    }
-        //    catch (Exception)
-        //    {
-        //        rta = false;
-        //    }
-        //    finally
-        //    {
-        //        if (this.conexion.State == ConnectionState.Open)
-        //        {
-        //            this.conexion.Close();
-        //        }
-        //    }
+                }
 
-        //    return rta;
-        //}
+                sql += "WHERE id = @id";
 
-        //#endregion
+                this.comando.CommandType = CommandType.Text;
+                this.comando.CommandText = sql;
+                this.comando.Connection = this.conexion;
 
-        //#region Delete
+                this.conexion.Open();
 
-        //public bool EliminarDato(int id)
-        //{
-        //    bool rta = true;
+                int filasAfectadas = this.comando.ExecuteNonQuery();
 
-        //    try
-        //    {
-        //        this.comando = new SqlCommand();
+                if (filasAfectadas == 0)
+                {
+                    rta = false;
+                }
 
-        //        this.comando.Parameters.AddWithValue("@id", id);
+            }
+            catch (Exception)
+            {
+                rta = false;
+            }
+            finally
+            {
+                if (this.conexion.State == ConnectionState.Open)
+                {
+                    this.conexion.Close();
+                }
+            }
 
-        //        string sql = "DELETE FROM dato ";
-        //        sql += "WHERE id = @id";
+            return rta;
+        }
 
-        //        this.comando.CommandType = CommandType.Text;
-        //        this.comando.CommandText = sql;
-        //        this.comando.Connection = this.conexion;
+        #endregion
 
-        //        this.conexion.Open();
+        #region Delete
 
-        //        int filasAfectadas = this.comando.ExecuteNonQuery();
+        public bool EliminarSistema(int id)
+        {
+            bool rta = true;
 
-        //        if (filasAfectadas == 0)
-        //        {
-        //            rta = false;
-        //        }
+            try
+            {
+                this.comando = new SqlCommand();
 
-        //    }
-        //    catch (Exception)
-        //    {
-        //        rta = false;
-        //    }
-        //    finally
-        //    {
-        //        if (this.conexion.State == ConnectionState.Open)
-        //        {
-        //            this.conexion.Close();
-        //        }
-        //    }
+                this.comando.Parameters.AddWithValue("@id", id);
 
-        //    return rta;
-        //}
+                string sql = "DELETE FROM tabla ";
+                sql += "WHERE id = @id";
+
+                this.comando.CommandType = CommandType.Text;
+                this.comando.CommandText = sql;
+                this.comando.Connection = this.conexion;
+
+                this.conexion.Open();
+
+                int filasAfectadas = this.comando.ExecuteNonQuery();
+
+                if (filasAfectadas == 0)
+                {
+                    rta = false;
+                }
+
+            }
+            catch (Exception)
+            {
+                rta = false;
+            }
+            finally
+            {
+                if (this.conexion.State == ConnectionState.Open)
+                {
+                    this.conexion.Close();
+                }
+            }
+
+            return rta;
+        }
 
         #endregion
 
--- Entidades/obj/Debug/net6.0/Entidades.AssemblyInfo.cs
+++ Entidades/obj/Debug/net6.0/Entidades.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entidades")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec0dac1a12694b8a9dffd59b65ed7b0de098f098")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5abf62b6077ddbc5cf0c9531731c6d7e8fd54d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entidades")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entidades")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
